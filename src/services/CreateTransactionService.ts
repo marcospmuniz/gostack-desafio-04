832b20bc8a929749cd035b9581a058057c165227
Marcos de P. Muniz <marcospmuniz@gmail.com>
2020-05-11 23:38:22 -0300

    conclusão do desafio de fundamentos de node.js

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('You do not have enough balance!');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type of transaction is invalid!');
+    }
+
+    return this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
   }
 }
 
